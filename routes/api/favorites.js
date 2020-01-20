85ea0d0ceb80d2f38917c50a054833554f6d9152
Jon Bent <jon.bent@me.com>
2020-01-20 12:08:12 -0800

    fix routes

--- routes/api/favorites.js
+++ routes/api/favorites.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
-
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const Favorite = require('../../models/Favorite');
+// router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+router.get('/meals', 
+    passport.authenticate('jwt', { session: false }), 
+    (req, res) => {
+        Favorite.find({})
+    }
+)
 module.exports = router;
