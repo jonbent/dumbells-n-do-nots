5005e4638fddd7f112ae3acbb38c860e799c7ce7
Jon Bent <jon.bent@me.com>
2020-01-21 10:54:34 -0800

    Add Navbar

---
+++ frontend/src/components/navbar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../scss/NavBar.scss'
+
+const NavBar = () => {
+    return (
+        <div className="navbar-container">
+            
+        </div>
+    )
+}
+
+export default NavBar
