a77c532c194e03b3f847709381aa22a401a2f7f7
Jon Bent <jon.bent@me.com>
2020-01-21 13:48:57 -0800

    Add Login for and styling for form

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 import NavBar from '../navbar/NavBar'
+
+import '../../scss/LoginForm.scss'
+
 export default class Login extends Component {
     render() {
         return (
-            <div>
+            <div className="login-page">
                 <NavBar/>
-                <div>
-                    
+                <div className="login-form-container">
+                    <form>
+                        <label>Username
+                            <input type="text"/>
+                        </label>
+                        <label>Password
+                            <input type="password"/>
+                        </label>
+                        <input type="submit"/>
+                    </form>
                 </div>
             </div>
         )
