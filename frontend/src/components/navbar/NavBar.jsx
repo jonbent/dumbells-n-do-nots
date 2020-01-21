4b6549b79f7d273d8b2ea7f6013304a6d96cd9b9
Jon <jon@cenports.com>
2020-01-21 13:48:57 -0800

    Add Login for and styling for form

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -4,7 +4,12 @@ import '../../scss/NavBar.scss'
 const NavBar = () => {
     return (
         <div className="navbar-container">
-            
+            <div className="navbar">
+                <div className="navbar-logo">
+                    {/* <img src="/images/donut-dumbbell-full-logo.png" alt=""/> */}
+                </div>
+                {/* <div className="navbar-name"><h3>Dumbells-N-Do Nots</h3></div> */}
+            </div>
         </div>
     )
 }
