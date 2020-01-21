02c12eea830e24ef392ab7747b054a5b76944ebe
Jon Bent <jon.bent@me.com>
2020-01-20 20:13:08 -0800

    Add Routes for Favorite, Muscles, and Muscle Groups. Add User auth token to sign up

--- routes/api/favorites.js
+++ routes/api/favorites.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const Favorite = require('../../models/Favorite');
-// router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
-router.get('/meals', 
+const validateFavorite = require('../../validation/favorite');
+router.get('/', 
     passport.authenticate('jwt', { session: false }), 
     (req, res) => {
-        Favorite.find({})
+        Favorite.find({ favoritable: req.body.favoritableId, favoritableModel: req.body.favoritableType, user: req.user._id})
+        .populate('meal')
+    }
+)
+
+router.post('/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateFavorite(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+        Favorite.find({ favoritable: req.body.favoritableId, favoritableModel: req.body.favoritableType }).then(favorite => {
+            if (favorite) {
+                errors.favoritableId = "Favorite Already Exists."
+                return res.status(400).json(errors);
+            }
+            Favorite.create({ favoritable: req.body.favoritableId, favoritableModel: req.body.favoritableType, user: req.user._id })
+        })
     }
 )
 module.exports = router;
