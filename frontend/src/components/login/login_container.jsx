19ddccdac6c9668e54e5407f8377104c039d6358
Manraj <manrajkhokhar1995@gmail.com>
2020-01-20 17:58:37 -0800

    add signup component

---
+++ frontend/src/components/login/login_container.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state, { location }) => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)();
