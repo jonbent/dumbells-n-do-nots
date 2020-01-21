f30d9c1c4cdde5f83fe99b81ab974ae5d0d61387
Modini <mmmymustafa@yahoo.com>
2020-01-21 12:13:06 -0800

    improved routes and more validations

--- models/UserWorkout.js
+++ models/UserWorkout.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserWorkoutSchema = new Schema({
+    routine: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "Routine"
+    },
     user: {
         type: Schema.Types.ObjectId,
         required: true,
