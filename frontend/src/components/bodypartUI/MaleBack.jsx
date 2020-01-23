194a62fe027bbf637ba44978f9e5ec0918be6a50
JuliusWu <aapple1994@gmail.com>
2020-01-23 14:26:09 -0800

    set up UI files

---
+++ frontend/src/components/bodypartUI/MaleBack.jsx
@@ -0,0 +1,31 @@
+import React from 'React';
+class MaleBack extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <img id="back-male-muscle-group" src="../../../public/images/maleUI/back/back-male-muscle-group.png"></img>
+
+                <img id="back-back" src="../../../public/images/maleUI/back/back-back.png"></img>
+
+                <img id="back-calves" src="../../../public/images/maleUI/back/back-calves.png"></img>
+
+            <div class="back-arms">
+                    <img id="back-left-arm" src="../../../public/images/maleUI/back/back-left-arm.png" onClick=""></img>
+                    <img id="back-right-arm" src="../../../public/images/maleUI/back/back-right-arm.png" onClick=""></img>
+            </div>
+
+            <div class="back-shoulder">
+                    <img id="back-top-shoulder" src="../../../public/images/maleUI/back/back-top-shoulder.png" onClick=""></img>
+                    <img id="back-left-shoulder" src="../../../public/images/maleUI/back/back-left-shoulder.png" onClick=""></img>
+                    <img id="back-right-shoulder" src="../../../public/images/maleUI/back/back-right-shoulder.png" onClick=""></img>
+            </div>
+
+            <img id="back-legs" src="images/back-legs.png"></img>
+            </div>
+        )
+    }
+
+}
+
+export default MaleBack; 
