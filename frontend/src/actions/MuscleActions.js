d42cf8707bd3b2c3daf4c10abf988b79d5a4393e
Manraj <manrajkhokhar1995@gmail.com>
2020-01-23 14:14:43 -0800

    add api util for muscles

---
+++ frontend/src/actions/MuscleActions.js
@@ -0,0 +1,18 @@
+import * as MusclesAPIUtil from '../util/musclesApiUtil';
+import jwt_decode from 'jwt-decode';
+
+
+export const RECEIVE_MUSCLES = 'RECEIVE_MUSCLES';
+
+const receiveMuscleGroups = (payload) => ({
+    type: RECEIVE_MUSCLES,
+    payload
+});
+
+export const fetchMuscles = () => dispatch => (
+    MusclesAPIUtil.fetchMusclesUtil()
+        .then(res => {
+            dispatch(receiveMuscleGroups(res))
+        })
+
+);
