d481a61804e0e9c82b5ab125db4d3a8522d5f536
Jon <jon@cenports.com>
2020-01-24 10:30:31 -0800

    Fix Routing issues, Add background to modal

--- frontend/src/components/home/Splash.jsx
+++ frontend/src/components/home/Splash.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
 const Splash = () => {
     return (
-        <div>
-            
-        </div>
+        <Redirect to="/login"/>
     )
 }
 
