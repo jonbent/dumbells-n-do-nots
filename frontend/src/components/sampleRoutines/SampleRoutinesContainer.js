4b2ac9a8dfbad530611ec7c1083b565d3edf9b26
Jon Bent <jon.bent@me.com>
2020-01-23 19:40:24 -0800

    Fix Modal Requirements

---
+++ frontend/src/components/sampleRoutines/SampleRoutinesContainer.js
@@ -0,0 +1,4 @@
+import { connect } from 'react-redux';
+import SampleRoutines from './SampleRoutines';
+
+export default connect()(SampleRoutines);
---
+++ frontend/src/components/workouts/AddWorkoutsFormContainer.js
@@ -0,0 +1,4 @@
+import { connect } from 'react-redux';
+import AddWorkoutsForm from './AddWorkoutsForm';
+
+export default connect()(AddWorkoutsForm);
