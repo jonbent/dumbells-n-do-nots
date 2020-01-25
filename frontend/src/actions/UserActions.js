b486673e765c036320ad3060ea6e114d5f8b7f93
Manraj <manrajkhokhar1995@gmail.com>
2020-01-24 17:18:10 -0800

    remove bug in update route

--- frontend/src/actions/UserActions.js
+++ frontend/src/actions/UserActions.js
@@ -1,4 +1,8 @@
 import * as UserApiUtil from '../util/UserApiUtil'
+import * as SessionApiUtil from "../util/SessionApiUtil";
+import jwt_decode from "jwt-decode";
+
+import {receiveCurrentUser, receiveErrors} from './SessionActions';
 
 export const RECEIVE_USER = 'RECEIVE_USER';
 
@@ -11,6 +15,17 @@ export const fetchUser = (username) => dispatch => (
     UserApiUtil.fetchUserInfo(username).then(payload => dispatch(receiveUser(payload)))
 )
 
-export const updateUser = (formData,username) => dispatch =>
-    UserApiUtil.updateUserInfo(formData, username)
-    .then(payload => dispatch(receiveUser(payload)))
+export const updateUser = (formData, username) => dispatch =>
+         UserApiUtil.updateUserInfo(formData, username)
+           .then(res => {
+             const { token } = res.data;
+             localStorage.setItem("jwtToken", token);
+             SessionApiUtil.setAuthToken(token);
+             const decoded = jwt_decode(token);
+             dispatch(receiveCurrentUser(decoded));
+            // console.log('res',res);
+            
+           })
+           .catch(err => {
+             dispatch(receiveErrors(err.response.data));
+           });
