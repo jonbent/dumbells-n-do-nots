dcfd58be503db1abb4a02c066f2586b6c56b3cb3
Manraj <manrajkhokhar1995@gmail.com>
2020-01-25 17:12:15 -0800

    add login errors

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -9,7 +9,8 @@ export default class Login extends Component {
     
         this.state = {
              username: "",
-             password: ""
+             password: "",
+             errors: this.props.errors
         }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleUpdate = this.handleUpdate.bind(this)
@@ -20,42 +21,53 @@ export default class Login extends Component {
     }
     handleSubmit(e){
         e.preventDefault();
-        this.props.login(this.state)
+        this.props.login(this.state).then(
+            () => this.props.history.push('/'),
+            () => this.props.history.push('/')
+        )
     }
-    
     render() {
-        const {username, password, errors} = this.state;
+        const {username, password} = this.state;
         let usernameError;
         let passwordError;
-        if(errors){
-            if(errors.username){
-                usernameError=errors.username;
+        if(this.state.errors){
+            if(this.state.errors.username){
+                usernameError=this.state.errors.username;
             }
-            if(errors.password){
-                passwordError=errors.password;
+            if(this.state.errors.password){
+                passwordError=this.state.errors.password;
             }
         }
         return (
-            <div className="login-page">
-                <NavBar/>
-                <div className="login-form-container">
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="selected-field">
-                            <label htmlFor="username">Username
-                            </label>
-                            <input type="text" id="username" value={username} onChange={e => this.handleUpdate("username", e)}/>
-                            {usernameError}
-                        </div>
-                        <div className="selected-field">
-                            <label htmlFor="password">Password
-                            </label>
-                            <input type="password" id="password" value={password} onChange={e => this.handleUpdate("password", e)}/>
-                            {passwordError}
-                        </div>
-                        <input type="submit" value="Log In"/>
-                    </form>
+          <div className="login-page">
+            l
+            <NavBar />
+            <div className="login-form-container">
+              <form onSubmit={this.handleSubmit}>
+                <div className="selected-field">
+                  <label htmlFor="username">Username</label>
+                  <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={e => this.handleUpdate("username", e)}
+                  />
                 </div>
+                {usernameError}
+                <div className="selected-field">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={e => this.handleUpdate("password", e)}
+                  />
+                </div>
+                {passwordError}
+                <input type="submit" value="Log In" />
+              </form>
             </div>
-        )
+          </div>
+        );
     }
 }
