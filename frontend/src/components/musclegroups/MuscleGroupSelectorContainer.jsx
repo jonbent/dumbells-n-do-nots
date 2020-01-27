48ba1d1a93724bb93db68ad92f621cd012908234
Modini <mmmymustafa@yahoo.com>
2020-01-26 17:55:38 -0800

    select meals is dope

--- frontend/src/components/musclegroups/MuscleGroupSelectorContainer.jsx
+++ frontend/src/components/musclegroups/MuscleGroupSelectorContainer.jsx
@@ -1,16 +1,16 @@
-import { connect } from 'react-redux';
-import MuscleGroupSelector from "./MuscleGroupSelector";
-import {fetchMuscleGroups} from '../../actions/MuscleGroupActions';
-import {fetchAllExercisesByMuscleGroup} from '../../actions/ExerciseActions';
+// import { connect } from 'react-redux';
+// import MuscleGroupSelector from "./MuscleGroupSelector";
+// import {fetchMuscleGroups} from '../../actions/MuscleGroupActions';
+// import {fetchAllExercisesByMuscleGroup} from '../../actions/ExerciseActions';
 
 
-const mSTP = (state) => ({
-        sex: state.session.user.sex
-})
+// const mSTP = (state) => ({
+//         sex: state.session.user.sex
+// })
 
-const mDTP = dispatch =>({
-    fetchMuscleGroups: () => dispatch(fetchMuscleGroups()),
-    fetchAllExercisesByMuscleGroup: () =>dispatch(fetchAllExercisesByMuscleGroup())
-})
+// const mDTP = dispatch =>({
+//     fetchMuscleGroups: () => dispatch(fetchMuscleGroups()),
+//     fetchAllExercisesByMuscleGroup: () =>dispatch(fetchAllExercisesByMuscleGroup())
+// })
 
-export default connect(mSTP, mDTP)(MuscleGroupSelector);
+// export default connect(mSTP, mDTP)(MuscleGroupSelector);
