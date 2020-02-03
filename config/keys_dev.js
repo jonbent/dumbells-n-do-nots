7be7f4c2fefe6250c5618ced4b6ce377bd465d0d
Modini <mmmymustafa@yahoo.com>
2020-02-03 11:28:14 -0800

    big thangs poppin and lil thangs steppin ayyyy

---
+++ config/keys_dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+        mongoURI: 'mongodb+srv://dev:SwumYinOvJXLtpmT@cluster0-foejx.mongodb.net/test?retryWrites=true&w=majority',
+        secretOrKey: 'Ko!0@+ckJAc#c!b',
+        awsBucketAccessId: 'AKIAT5GMY7SE3QHKEVF7',
+        awsBucketToken: '7O+pUzoblkAvuemKWW42A0n5eGLbWAzHYiYqjn0n',
+        awsRegion: 'us-west-1',
+        UploadFileUrlLink: 'https://s3.us-west-1.amazonaws.com/dumbells-n-donuts-dev/',
+        awsBucketName: 'dumbells-n-donuts-dev'
+}
