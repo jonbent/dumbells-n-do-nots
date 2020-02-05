e2b58ca822e0a5896ee403cced2d9da195871556
Jon Bent <jon.bent@me.com>
2020-02-05 11:18:24 -0800

    Add Pagination

--- frontend/src/components/userMeals/AddUserMealsForm.jsx
+++ frontend/src/components/userMeals/AddUserMealsForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Pagination from '../pagination/Pagination'
 class AddUserMealsForm extends React.Component{
     constructor(props){
         super(props)
@@ -8,10 +8,17 @@ class AddUserMealsForm extends React.Component{
             day: Object.keys(this.props.daySelect)[dayStr],
             numMeals: 0,
             toggleShowMeals: false,
+            curPage: 1,
+            pageSize: 10,
+            itemsAmount: 100,
         }
         this.updateField = this.updateField.bind(this);
         this.handleSetDate = this.handleSetDate.bind(this)
         this.handleSetNumMeals = this.handleSetNumMeals.bind(this)
+        this.handlePageChange = this.handlePageChange.bind(this)
+    }
+    handlePageChange(page){
+        this.setState({curPage: page});
     }
 
     componentDidMount(){
@@ -37,9 +44,20 @@ class AddUserMealsForm extends React.Component{
             if(numMeals){
             let numMealsArray = [...Array(numMeals).keys()]
             meals = (
-                <ul>
-                    {this.props.meals.data.map(meal => <li><img src={meal.image}/><div>{meal.title}{meal.calories}{meal.fat}{meal.carbs}{meal.protein}</div></li>)}
-                </ul>
+                <div>
+                    <ul>
+                        {this.props.meals.data.map(meal => <li><img src={meal.image}/><div>{meal.title}{meal.calories}{meal.fat}{meal.carbs}{meal.protein}</div></li>)}
+                    </ul>
+                    <div className="pagination-container">
+                        <Pagination
+                            changePage={this.handlePageChange}
+                            curPage={this.state.curPage}
+                            pageSize={this.state.pageSize}
+                            itemsAmount={this.state.itemsAmount}
+                        />
+
+                    </div>
+                </div>
             );
             } else {
                 this.setState({toggleShowMeals: false})
