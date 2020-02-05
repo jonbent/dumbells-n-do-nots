eed2d7720267d2cce8c07d807ec172e65b53929e
Jon <jon@cenports.com>
2020-02-05 11:18:24 -0800

    Add Pagination

---
+++ frontend/src/components/pagination/OutsideClickHandler.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class OutsideClickHandler extends Component {
+    constructor(props) {
+        super(props)
+
+        this.setRef = this.setRef.bind(this);
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    }
+    componentDidMount() {
+        document.addEventListener('mouseup', this.handleOutsideClick);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mouseup', this.handleOutsideClick);
+    }
+
+    setRef(node){
+        this.ref = node;
+    }
+
+    handleOutsideClick(e, ...options) {
+        if (this.ref && !this.ref.contains(e.target)) {
+            this.props.action(...options);
+        }
+    }
+
+    render() {
+        const className = this.props.className ? this.props.className : ""
+        return (
+            <div ref={this.setRef} className={className}>{this.props.children}</div>
+        );
+    }
+}
