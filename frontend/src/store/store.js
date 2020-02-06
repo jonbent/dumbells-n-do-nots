744660bab839f8927aa8c540083fb1d495492f22
Jon Bent <jon.bent@me.com>
2020-02-05 16:55:36 -0800

    Add Persist, Add logic for meals in newroutinedata

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,14 +2,26 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import rootReducer from '../reducers/RootReducer';
 
-const configureStore = (preloadedState = {}) => (
-    createStore(
-        rootReducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const configureStore = (preloadedState = {}) => {
+    let store = createStore(
+        persistedReducer,
         preloadedState,
         applyMiddleware(thunk, logger)
     )
-);
+    let persistor = persistStore(store);
+    return {store, persistor}
+};
 
 export default configureStore;
